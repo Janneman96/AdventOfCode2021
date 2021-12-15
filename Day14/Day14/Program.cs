b044e11b58f09bb655a0f1f017eb25b06ae7b1df
Janneman96 <ikjman@hotmail.com>
2021-12-16 00:30:33 +0100

    Improved performance by using delegates

--- Day14/Day14/Program.cs
+++ Day14/Day14/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Day14
 {
     class Program
     {
-        static void Main()
+                static void Main()
         {
-            var inputLines = System.IO.File.ReadAllLines($@"C:\git\adventofcode2021\Day14\Day14\example.txt");
+            var inputLines = System.IO.File.ReadAllLines($@"C:\git\adventofcode2021\Day14\Day14\input.txt");
 
             var polymerTemplate = inputLines.First();
             Console.WriteLine($"Template: {polymerTemplate}");
@@ -15,62 +16,70 @@ namespace Day14
             var pairInsertionRules = inputLines
                 .Skip(2)
                 .Select(inputLine =>
-                    new PairInsertionRule
+                    new PairInsertionCalculation
                     {
                         From = inputLine.Split(" -> ")[0],
-                        To = inputLine.Split(" -> ")[1]
+                        To = inputLine.Split(" -> ")[1][0]
                     }).ToList();
 
+            var pairInsertionCalculations = new List<PairInsertionCalculation>();
+            var characterCounts = new Dictionary<char, ulong>();
 
-
-            var temporaryPolymerTemplate = string.Empty;
-            for (var step = 1; step <= 100; step++)
+            foreach (var rule in pairInsertionRules)
             {
-                for (var i = 0; i < polymerTemplate.Length - 1; i++)
+                pairInsertionCalculations.Add(new PairInsertionCalculation { From = rule.From, To = rule.To });
+                if (!characterCounts.ContainsKey(rule.To))
                 {
-                    var insertionRule = pairInsertionRules.SingleOrDefault(rule => rule.From == polymerTemplate.Substring(i, 2));
-                    if (insertionRule != null)
-                    {
-                        temporaryPolymerTemplate += insertionRule.To;
-                    }
+                    characterCounts.Add(rule.To, 0);
                 }
-                polymerTemplate = polymerTemplate[0] + temporaryPolymerTemplate;
-                temporaryPolymerTemplate = string.Empty;
-
-                //Console.WriteLine($"After step {step}: {polymerTemplate}");
-                //Console.WriteLine($"After step {step}: {polymerTemplate.Length}");
-                //Console.WriteLine($"Processed step {step}");
-                var ruleUsedCount = pairInsertionRules.Count(rule => polymerTemplate.Contains(rule.From));
-                Console.WriteLine($"contains {ruleUsedCount} rules. Rules unused: {string.Join(',', pairInsertionRules.Where(rule => !polymerTemplate.Contains(rule.From)).Select(rule => rule.From))}");
             }
-            var maxChar = polymerTemplate.GroupBy(character => character).Max(group => group.Count());
-            var minChar = polymerTemplate.GroupBy(character => character).Min(group => group.Count());
 
-            Console.WriteLine("max " + maxChar);
-            Console.WriteLine("min " + minChar);
-            Console.WriteLine(maxChar - minChar);
+            const int steps = 20;
+
+            var completed20StepCalculations = new Dictionary<string, ulong>();//todo use
+
+            foreach (var calculation in pairInsertionCalculations)
+            {
+                calculation.NextLeft = pairInsertionCalculations.Single(c => c.From == string.Join(string.Empty, calculation.From[0], calculation.To));
+                calculation.NextRight = pairInsertionCalculations.Single(c => c.From == string.Join(string.Empty, calculation.To, calculation.From[1]));
+                calculation.GetCharacterCountsForSteps = (stepsRemaining) =>
+                {
+                    if (stepsRemaining == steps)
+                    {
+                        Console.WriteLine($"Calculating {calculation.From}");
+                    }
+                    if (stepsRemaining == 1)
+                    {
+                        characterCounts[calculation.To]++;//todo use reference to completed20StepCalculations, which gets passed through the 
+                        characterCounts[calculation.From[1]]++;
+                    }
+                    else
+                    {
+                        calculation.NextLeft.GetCharacterCountsForSteps(stepsRemaining - 1);
+                        calculation.NextRight.GetCharacterCountsForSteps(stepsRemaining - 1);
+                    }
+                };
+            }
+
+            for (var i = 0; i < polymerTemplate.Length - 1; i++)
+            {
+                var insertionRule = pairInsertionCalculations.SingleOrDefault(calculation => calculation.From == polymerTemplate.Substring(i, 2));
+                insertionRule.GetCharacterCountsForSteps(steps);
+            }
+
+            characterCounts[polymerTemplate.First()]++;
+
+            Console.WriteLine(string.Join(",", characterCounts));
         }
 
-        class PairInsertionRule
+        class PairInsertionCalculation
         {
-            private char to { get; set; }
             public string From { get; set; }
-            public string To
-            {
-                get
-                {
-                    return string.Join(
-                        string.Empty,
-                        new char[] {
-                            //From[0],
-                            to,
-                            From[1] });
-                }
-                set
-                {
-                    to = value[0];
-                }
-            }
+            public char To { get; set; }
+            public Action<int> GetCharacterCountsForSteps { get; set; }
+            public ulong CalculateTwentySteps { get; set; }
+            public PairInsertionCalculation NextLeft { get; set; }
+            public PairInsertionCalculation NextRight { get; set; }
         }
     }
 }
