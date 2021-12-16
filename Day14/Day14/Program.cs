bad4096c031359a3cf55e298cdb6b525eb1aa716
Janneman96 <ikjman@hotmail.com>
2021-12-16 21:57:28 +0100

    Day 14 completed.

--- Day14/Day14/Program.cs
+++ Day14/Day14/Program.cs
@@ -6,9 +6,11 @@ namespace Day14
 {
     class Program
     {
-                static void Main()
+        static void Main()
         {
+            var datetimeStart = DateTime.Now;
             var inputLines = System.IO.File.ReadAllLines($@"C:\git\adventofcode2021\Day14\Day14\input.txt");
+            const int steps = 40;
 
             var polymerTemplate = inputLines.First();
             Console.WriteLine($"Template: {polymerTemplate}");
@@ -23,63 +25,99 @@ namespace Day14
                     }).ToList();
 
             var pairInsertionCalculations = new List<PairInsertionCalculation>();
-            var characterCounts = new Dictionary<char, ulong>();
 
             foreach (var rule in pairInsertionRules)
             {
                 pairInsertionCalculations.Add(new PairInsertionCalculation { From = rule.From, To = rule.To });
-                if (!characterCounts.ContainsKey(rule.To))
-                {
-                    characterCounts.Add(rule.To, 0);
-                }
             }
-
-            const int steps = 20;
-
-            var completed20StepCalculations = new Dictionary<string, ulong>();//todo use
 
             foreach (var calculation in pairInsertionCalculations)
             {
                 calculation.NextLeft = pairInsertionCalculations.Single(c => c.From == string.Join(string.Empty, calculation.From[0], calculation.To));
                 calculation.NextRight = pairInsertionCalculations.Single(c => c.From == string.Join(string.Empty, calculation.To, calculation.From[1]));
-                calculation.GetCharacterCountsForSteps = (stepsRemaining) =>
+            }
+
+            var calculationResults = new List<Dictionary<char, ulong>>();
+            for (var i = 0; i < polymerTemplate.Length - 1; i++)
+            {
+                var insertionCalculation = pairInsertionCalculations.SingleOrDefault(calculation => calculation.From == polymerTemplate.Substring(i, 2));
+                calculationResults.Add(insertionCalculation.GetCharacterCountsForSteps(steps));
+            }
+
+            var characterCounts = new Dictionary<char, ulong>();
+
+            foreach (var result in calculationResults)
+            {
+                foreach (var charResult in result)
                 {
-                    if (stepsRemaining == steps)
+                    if (!characterCounts.ContainsKey(charResult.Key))
                     {
-                        Console.WriteLine($"Calculating {calculation.From}");
-                    }
-                    if (stepsRemaining == 1)
-                    {
-                        characterCounts[calculation.To]++;//todo use reference to completed20StepCalculations, which gets passed through the 
-                        characterCounts[calculation.From[1]]++;
+                        characterCounts.Add(charResult.Key, charResult.Value);
                     }
                     else
                     {
-                        calculation.NextLeft.GetCharacterCountsForSteps(stepsRemaining - 1);
-                        calculation.NextRight.GetCharacterCountsForSteps(stepsRemaining - 1);
+                        characterCounts[charResult.Key] += charResult.Value;
                     }
-                };
+                }
             }
-
-            for (var i = 0; i < polymerTemplate.Length - 1; i++)
-            {
-                var insertionRule = pairInsertionCalculations.SingleOrDefault(calculation => calculation.From == polymerTemplate.Substring(i, 2));
-                insertionRule.GetCharacterCountsForSteps(steps);
-            }
-
             characterCounts[polymerTemplate.First()]++;
 
             Console.WriteLine(string.Join(",", characterCounts));
+            Console.WriteLine($"{steps} steps completed in {(DateTime.Now - datetimeStart).Milliseconds} milliseconds.");
+
+            Console.WriteLine(characterCounts.Max(characterCount => characterCount.Value) - characterCounts.Min(characterCount => characterCount.Value));
         }
 
         class PairInsertionCalculation
         {
             public string From { get; set; }
             public char To { get; set; }
-            public Action<int> GetCharacterCountsForSteps { get; set; }
             public ulong CalculateTwentySteps { get; set; }
             public PairInsertionCalculation NextLeft { get; set; }
             public PairInsertionCalculation NextRight { get; set; }
+            private Dictionary<int, Dictionary<char, ulong>> Solutions { get; set; } = new Dictionary<int, Dictionary<char, ulong>>();
+            public Dictionary<char, ulong> GetCharacterCountsForSteps(int steps)
+            {
+                if (Solutions.ContainsKey(steps))
+                {
+                    return Solutions[steps];
+                }
+                var solution = new Dictionary<char, ulong>();
+                if (steps == 1)
+                {
+                    solution.Add(To, 1);
+                    if (!solution.ContainsKey(From[1]))
+                    {
+                        solution.Add(From[1], 1);
+                    }
+                    else
+                    {
+                        solution[From[1]]++;
+                    }
+                }
+                else
+                {
+                    var leftSolutions = NextLeft.GetCharacterCountsForSteps(steps - 1);
+                    var rightSolutions = NextRight.GetCharacterCountsForSteps(steps - 1);
+                    foreach (var leftSolution in leftSolutions)
+                    {
+                        solution.Add(leftSolution.Key, leftSolution.Value);
+                    }
+                    foreach (var rightSolution in rightSolutions)
+                    {
+                        if (!solution.ContainsKey(rightSolution.Key))
+                        {
+                            solution.Add(rightSolution.Key, rightSolution.Value);
+                        }
+                        else
+                        {
+                            solution[rightSolution.Key] += rightSolution.Value;
+                        }
+                    }
+                }
+                Solutions.Add(steps, solution);
+                return solution;
+            }
         }
     }
 }
